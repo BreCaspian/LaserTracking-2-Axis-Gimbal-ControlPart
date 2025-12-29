de4b7535e0e636bcc372556825ef751da8b9f427
Cofallen <1727334304@qq.com>
2025-12-30 00:09:02 +0900

    任务调用优化

--- User/Start/All_Task.c
+++ User/Start/All_Task.c
@@ -38,7 +38,7 @@ void StartGimbalTask(void)
                         VisionRxData.Data.VisionState,
                         0);
         gimbal_task(&ALL_CONTAL, &ALL_MOTOR);
-        // RobotTask(2, &DBUS_V_DATA, &ALL_CONTAL);  // 3 自瞄 2 遥控
+        RobotTask(3, &DBUS_V_DATA, &ALL_CONTAL);  // 3 自瞄 2 遥控
         osDelay(1);
     }
 }
@@ -49,7 +49,7 @@ void StartMonitorTask(void)
     for(;;)
     {
         ALL_CONTAL.DWT_TIME.Monitor_dt = DWT_GetDeltaT(&ALL_CONTAL.DWT_TIME.Monitor_Count);
-        RobotTask(3, &DBUS_V_DATA, &ALL_CONTAL);  // 3 自瞄 2 遥控
+        // RobotTask(3, &DBUS_V_DATA, &ALL_CONTAL);  // 3 自瞄 2 遥控
         DBUS_OFFLINE_Check(&DBUS_V_DATA);           // can发送在这里，应该写到task里的，懒了
         Vision_Monitor(&VisionRxData);
         osDelay(1);
